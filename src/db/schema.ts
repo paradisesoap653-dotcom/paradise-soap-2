aaf97a44ee37718ec41cdd2c0a7718a1d873b38b
paradisesoap653-dotcom <paradisesoap653@gmail.com>
2026-07-16 15:59:14 +0200

    Set up database connection with drizzle-orm

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,1 +1,9 @@
+om "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+export const db = drizzle(pool, { schema });
