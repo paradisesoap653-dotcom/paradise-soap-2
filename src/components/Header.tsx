96b0f9c218d6330f9e56bc9b3c09d12fdd9ada64
paradisesoap653-dotcom <paradisesoap653@gmail.com>
2026-07-22 09:37:55 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -34,7 +34,7 @@ export default function Header() {
       <div className="mx-auto flex max-w-6xl items-center justify-between gap-4 px-6 py-4">
         <Link href="/" className="flex items-center">
           <Image
-            src="/logo.png.jpg"
+            src="/shoplogo.jpg.jpg"
             alt="Paradise Soap"
             width={48}
             height={48}
